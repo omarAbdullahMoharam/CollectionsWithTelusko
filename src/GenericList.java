6e4e7e68e96e388a95d4de408e0ce20f41c5bc0b
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-01-22 00:13:13 +0200

    Add constructors to GenericList and improve item addition logic

--- src/GenericList.java
+++ src/GenericList.java
@@ -1,20 +1,24 @@
+
+
 import java.util.Iterator;
 
 public class GenericList <T>implements Iterable<T> {
     private T[] items = (T[]) new Object[10];
+    public GenericList() {
+    }
+    public GenericList(T[] items) {
+        this.items = items;
+    }
     private int count ;
     public void add(T item){
         if (count == items.length){
-            T[] newItems = null;
-            try {
-                newItems = (T[]) new Object[items.length * 2];
+            T[] newItems = (T[]) new Object[items.length * 2] ;
                 System.arraycopy(items, 0, newItems, 0, items.length);
                 items = newItems;
                 items[count++] = item;
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+        }
+        else {
+            items[count++] = item;
         }
     }
 
@@ -45,5 +49,24 @@ public class GenericList <T>implements Iterable<T> {
         public T next() {
             return list.get(index++);
         }
+    }
+
+    public static void main(String[] args) {
+       var stringList = new GenericList<String>();
+        stringList.add("Hello");
+        stringList.add("World");
+
+        for (String s : stringList) {
+            System.out.println(s);
+        }
+
+        var intList = new GenericList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        for (Integer i : intList) {
+            System.out.println(i);
+        }
+
     }
 }
